87e37156fb03dea9020aca842441f49806567229
WindiLukman <windi.lukman@student.sgu.ac.id>
2023-11-20 18:09:56 +0700

    Feat(Hooks) and Feat: Local Storage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,13 @@ import Button from "react-bootstrap/Button";
 import "./App.css";
 import icon128 from "./assets/images/icon128.png";
 import { useEffect, useState } from "react";
+import useGetWeather from "./hooks/useGetWeather";
+import useCityStorage from "./hooks/useCityStorage";
 
 function App() {
     const [cityInput, setCityInput] = useState("");
+    const { getStorage, setStorage } = useCityStorage();
+    const { data: weatherData, getWeather } = useGetWeather();
 
     // Listens on changes in cityInput and then performs the function as intended
     // This hook depends on the deps variable to run the func
@@ -16,13 +20,23 @@ function App() {
     // This hook runs on an empty arrayed dep
     // This hook will only run once the page is loaded
     useEffect(() => {
-        console.log("This page is loaded");
+        //console.log("This page is loaded");
+        //console.log(getStorage());
     }, []);
 
     // This hook runs once any state is updated
     useEffect(() => {
-        console.log("Everything else is updated");
+        //console.log("Everything else is updated");
     });
+
+    const onSubmitButtonClicked = () => {
+        getWeather(cityInput);
+        setStorage(cityInput);
+    };
+
+    useEffect(() => {
+        //console.log(weatherData);
+    }, [weatherData]);
 
     return (
         <div class="container">
@@ -53,9 +67,7 @@ function App() {
                             }}
                         />
                         <Button
-                            onClick={() => {
-                                alert("This function is not available yet");
-                            }}
+                            onClick={onSubmitButtonClicked}
                             id="submitButton"
                             type="submit"
                         >
@@ -79,23 +91,31 @@ function App() {
                 <span class="sr-only">Loading...</span>
             </div>
 
-            <div id="weatherInfoComponent" style={{ display: "none" }}>
+            <div id="weatherInfoComponent" style={{ display: "flex" }}>
                 <table class="centerContainer" cellpadding="5px">
                     <tr>
                         <th class="customTableHeader">Temperature</th>
-                        <td id="temperatureField">0</td>
+                        <td id="temperatureField">
+                            {`${weatherData?.main?.temp ?? "-"}°C`}
+                        </td>
                     </tr>
                     <tr>
                         <th class="customTableHeader">Feels Like</th>
-                        <td id="feelsLikeField">0</td>
+                        <td id="feelsLikeField">
+                            {`${weatherData?.main?.feels_like ?? "-"}°C`}
+                        </td>
                     </tr>
                     <tr>
                         <th class="customTableHeader">Humidity</th>
-                        <td id="humidityField">0</td>
+                        <td id="humidityField">{`${
+                            weatherData?.main?.humidity ?? "-"
+                        } %`}</td>
                     </tr>
                     <tr>
                         <th class="customTableHeader">Atmospheric Pressure</th>
-                        <td id="pressureField">0</td>
+                        <td id="pressureField">{`${
+                            weatherData?.main?.pressure ?? "-"
+                        } hPa`}</td>
                     </tr>
                 </table>
             </div>
